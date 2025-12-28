4f2b4df247ef02d5d500cc4a39edccbab5664997
Nagyun <soahtmvhswl@gmail.com>
2025-12-28 14:34:55 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <cstring>
 using namespace std;
 
 int cnt1 = 0;
 int cnt2 = 0;
-
+int N;
 char area[101][101];
 int visit[101][101] = { 0, };
+queue<pair<int, int>>q;
 
 int direct[4][2] = {
     -1,0, //상
@@ -17,59 +18,25 @@ int direct[4][2] = {
 };
 
 // 정상인인 경우 dfs
-void dfs(int y, int x) {
-    
-    visit[y][x] = 1;
- 
-
-    for (int i = 0; i < 4; i++) {
-        int dy = y + direct[i][0];
-        int dx = x + direct[i][1];
-
-        if (!visit[dy][dx] && area[dy][dx] == area[y][x]) {
-            visit[dy][dx] = 1;//방문처리
-            dfs(dy, dx);
-        }
-
-    }
-
-}
-
-// 적록색맹인 경우 dfs
-void dfs2(int y, int x) {
-
+void bfs(int y, int x) {
     visit[y][x] = 1;
 
+    q.push({ y,x });
+    while (!q.empty()) {
+        int yy = q.front().first;
+        int xx = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int dy = yy + direct[i][0];
+            int dx = xx + direct[i][1];
 
-    for (int i = 0; i < 4; i++) {
-        int dy = y + direct[i][0];
-        int dx = x + direct[i][1];
-
-        if (!visit[dy][dx]) 
-        {
-            // 탐색 시작점이 R인 경우
-            if (area[y][x] == 'R') {
-                if ((area[dy][dx] == 'R') || area[dy][dx] == 'G') {
-                    visit[dy][dx] = 1;//방문처리
-                    dfs2(dy, dx);
-                }
-            }
-            //  G인경우
-            else if (area[y][x] == 'G') {
-                
-                if (area[dy][dx] == 'G' || area[dy][dx] == 'R') {
-                    visit[dy][dx] = 1;//방문처리
-                    dfs2(dy, dx);
-                }
-            }
-            else { 
-                // B인경우
-                if (area[dy][dx] == area[y][x]) {
+            if (dy >= 0 && dy < N && dx >=0 && dx < N) {
+                if (!visit[dy][dx]&&area[dy][dx]==area[y][x]) {
                     visit[dy][dx] = 1;
-                    dfs2(dy, dx);
+                    q.push({ dy,dx });
                 }
             }
-                  
+
         }
 
     }
@@ -78,7 +45,7 @@ void dfs2(int y, int x) {
 int main() {
 
     //입력 처리
-    int N;
+    
     cin >> N;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -90,7 +57,7 @@ int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (!visit[i][j]) {
-                dfs(i, j);
+                bfs(i, j);
                 cnt1++;
             }
         }
@@ -98,11 +65,16 @@ int main() {
 
     memset(visit, 0, sizeof(visit));
 
-    // 적록색맹인 경우 : cnt2에 결과 저장
+    // 적록색맹인 경우 : R=G
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (area[i][j] == 'G')area[i][j] = 'R';
+        }
+    }
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (!visit[i][j]) {
-                dfs2(i, j);
+                bfs(i, j);
                 cnt2++;
             }
         }

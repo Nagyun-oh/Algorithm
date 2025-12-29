6306900f97cea045759220b671a43aecf1ca8a74
Nagyun <soahtmvhswl@gmail.com>
2025-12-29 11:32:35 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -1,66 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
 
 vector<int> map(101, 0);
 vector<int> visit(101, 0);
 
-queue<pair<int, int>>q;
-
-// 모든 칸은 최대 하나의 사다리 또는 뱀을 가지고 있으며, 동시에 두 가지를 가지는 경우는 없음
-// 항상 100번칸에 도착할 수 있는 입력만 주어진다.
-// 100번 칸에 도착하기 위해 주사위를 "최소" 몇번 굴려야 하는지 구하라. 
-
-void bfs(int start,int c) {
-	
-	q.push({ start,c });
+void bfs(int x,int c) {
+	queue<pair<int,int>>q;
+	q.push({ x,c });
 	while (!q.empty()) {
-		int pos = q.front().first;
-		int cnt = q.front().second;
+		int cur = q.front().first; // 현재 지점
+		int cnt = q.front().second; // 현재 횟수
 		q.pop();
-
 		for (int i = 1; i <= 6; i++) {
-			int dpos = pos + i;
-			if (dpos == 100) {
+			int next = cur + i;
+			if (next == 100) {
+				// 도착점인 경우 출력 후 종료
 				cout << cnt + 1;
 				return;
 			}
-			else if (dpos < 100) {
-				if (map[dpos] != 0) {
-					dpos = map[dpos];
+			else if (next < 100) {
+				// 사다리 또는 뱀인경우 위치 갱신
+				if (map[next] != 0) { // 이떄 if 를 while로 바꿔써도 상관없다
+					next = map[next];  // 어차피 각 지점에는 사다리 또는 뱀만 존재하므로, 사다리를 타고 다시 뱀을 연쇄적으로 타는 경우는 없다.
 				}
-				if (!visit[dpos]) {
-					visit[dpos] = 1;
-					q.push({ dpos,cnt+1 });
+				if (!visit[next]) {
+					visit[next] = 1; // 다음에 방문하면 어차피 최소가 아니므로  방문처리를 한다.
+					q.push({ next,cnt + 1 });
 				}
 			}
 		}
 	}
 
+
 }
 
-int main()
+int main() 
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	int N, M; // 사다리의 수 , 뱀의 수
+	int N, M;
 	cin >> N >> M;
 
-	// 뱀과 사디리 입력
-	int x, y; 
-	for (int i = 0; i < N + M; i++) {
+	int x, y;
+	for (int i = 0; i < N; i++) {
 		cin >> x >> y;
 		map[x] = y;
 	}
+	int u, v;
+	for (int i = 0; i < M; i++) {
+		cin >> u >> v;
+		map[u] = v;
+	}
 
-	 // bfs가 level단위로 탐색하여, 최소 주사위 횟수를 구하는 것을 보장하려면
-	 // cnt는 탐색 방향별로 따로 저장해야함
-	 int count = 0; 
-	 bfs(1,count);
-	 
-	
-	return 0;
+	bfs(1, 0);
+
 }

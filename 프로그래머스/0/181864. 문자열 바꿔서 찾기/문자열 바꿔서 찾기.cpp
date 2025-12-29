c2437269c8eca8efa969f194f3020f488d28bd32
Nagyun <soahtmvhswl@gmail.com>
2025-12-29 10:36:09 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for(int i=0;i<myString.length();i++){
+        if(myString[i]=='A')myString[i] = 'B';
+        else if(myString[i]=='B')myString[i] = 'A';
+    }
+    
+    if(myString.find(pat) != string::npos) answer = 1;
+    else answer = 0;
+    
+    return answer;
+}

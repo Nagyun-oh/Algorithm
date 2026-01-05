67ea64eeaa7fce7376e4c9e2d68a7856dcbbe3a5
Nagyun <soahtmvhswl@gmail.com>
2026-01-05 10:39:20 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    
+    for(int i=0;i<myString.size();i++){
+        if(myString[i]>='A'&&myString[i]<='Z'){
+            myString[i] += 32;
+        }
+    }
+    return myString;
+}

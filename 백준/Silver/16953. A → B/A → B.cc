0a4c4a13d681af7668f2d18b4ff61ba318836ead
Nagyun <soahtmvhswl@gmail.com>
2026-01-09 11:21:14 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,40 +1,32 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
 using namespace std;
 
-int A, B;
-queue<pair<long, int>>q;
+int main() {
 
-void bfs()
-{
-	q.push({ A,1 });
-	while (!q.empty()) {
-		long Res = q.front().first;
-		int cnt = q.front().second;
-		q.pop();
+	int a, b;
+	cin >> a >> b;
 
-		long multiRes = Res * 2;
-		long addOne = Res*10 + 1;
-
-		if (multiRes == B || addOne == B) {
-			cout << cnt+1;
-			return;
+	int result = 1;
+	while (true) {
+		if (a == b)break;
+		else if (a > b) {
+			result = -1;
+			break;
 		}
 
-		if(multiRes<B)q.push({ multiRes,cnt + 1 });
-		if (addOne < B)q.push({ addOne,cnt + 1 });
+		if (b % 2 == 0)b /= 2;
+		else if (b % 10 == 1) b = (b - 1) / 10;
+		else {
+			cout << -1;
+			return 0;
+		}
+		result++;
 
 	}
 
-	cout << -1;
-}
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	
-	cin >> A >> B;
-	bfs();
-	
+	cout << result;
+
+	return 0;
+
 }

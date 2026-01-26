2ed4852bc6ffbd78af58ca9bd3941ff7b947d8fb
Nagyun <soahtmvhswl@gmail.com>
2026-01-26 11:08:18 +0900

    [level 0] Title: 코드 처리하기, Time: 0.42 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    int mode = 0;
+    for(int idx=0;idx<code.size();idx++)
+    {
+       if(mode==0)
+       {
+           if(code[idx]!='1'&&idx%2==0)
+           {
+               answer +=code[idx];
+           }
+           else if(code[idx]=='1')
+           {
+                mode = 1;   
+           } 
+       }
+       else if (mode==1)
+       {   
+           if(code[idx]!='1'&&idx%2==1)
+           {
+               answer +=code[idx];
+           }
+           else if(code[idx]=='1')
+           {
+               mode = 0;
+           }           
+       }    
+    }
+    
+    if(answer.empty())answer ="EMPTY";
+    
+    return answer;
+}

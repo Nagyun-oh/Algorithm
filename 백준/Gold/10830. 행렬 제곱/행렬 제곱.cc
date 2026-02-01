6e169cf689ce044d4adadddfe536881dfd1c8be3
Nagyun <soahtmvhswl@gmail.com>
2026-02-01 17:12:56 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
 
-long long N, B;
-long long matrix[6][6];
-long long ans[6][6];
+int N;
+long long B;
+long long matrix[5][5];
+long long ans[5][5];
 
-void multiMatrix(long long matrix1[6][6], long long matrix2[6][6]) {
-	long long tmp[6][6];
+void input() {
+	cin >> N >> B;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			cin >> matrix[y][x];
+		}
+		ans[y][y] = 1; // 단위 벡터 입력
+	}
 
+}
+
+void multiMatrix(long long matrix1[5][5], long long matrix2[5][5]) {
+	long long tmp[5][5];
+	
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
 			long long sum = 0;
-
 			for (int i = 0; i < N; i++) {
 				sum += matrix1[y][i] * matrix2[i][x];
 			}
@@ -27,21 +38,10 @@ void multiMatrix(long long matrix1[6][6], long long matrix2[6][6]) {
 
 }
 
-void init() {
-	cin >> N >> B;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			cin >> matrix[y][x];
-		}
-		ans[y][y] = 1; // 단위 행렬 입력 A * I = A
-	}
-}
-
 void solve() {
 	while (B > 0) {
-		if (B % 2 == 1)multiMatrix(ans, matrix);  // A^2 * A
-
-		multiMatrix(matrix, matrix); // A^2
+		if (B % 2 == 1)multiMatrix(ans, matrix);
+		multiMatrix(matrix, matrix);
 		B /= 2;
 	}
 }
@@ -58,9 +58,10 @@ void printMatrix() {
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
-	
-	init();
+
+	input();
 	solve();
 	printMatrix();
 	
+
 }

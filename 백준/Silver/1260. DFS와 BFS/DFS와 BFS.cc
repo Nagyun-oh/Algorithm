7a49dc477af665bc1761562ae81eeb0656d29017
Nagyun <soahtmvhswl@gmail.com>
2026-02-26 10:36:16 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,108 +1,75 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <cstring>
+#include <vector>
 #include <algorithm>
-
 using namespace std;
 
-int N, M, V;  // 정점의 개수 N , 간선의 개수 M, 탐색을 시작할 정점의 번호 V
-vector<int> v[10001];
-bool visit[1001];
 
-vector<int> dfs;
-vector<int>bfs;
+int n, m;
+int v;
+vector<int> graph[1001];
+vector<int> visited;
 
-void init();
-void DFS(int start);
-void BFS(int start);
+void dfs(int curr) {
 
-void init()
-{
-	cin >> N >> M >> V;
-	int a, b;
-	for (int i = 1; i <= M; i++)
-	{
-		cin >> a >> b;
-		v[a].push_back(b); // 양방향 구현
-		v[b].push_back(a); // 양방형 구현
-	}
+	visited[curr] = 1;
 
-	// 낮은 숫자부터 탐색 (어차피 양방향이라 다 연결되있음)
-	for (int i = 1; i <= N; i++)
-	{
-		sort(v[i].begin(), v[i].end());
-	}
+	cout << curr << " ";
 
+	for (auto nx : graph[curr]) {
 
-
-}
-
-// DFS  -> 재귀로 구현
-void DFS(int start)
-{
-	visit[start] = true; //  방문처리
-	dfs.push_back(start); // dfs 경로 저장
-
-	for (int i = 0; i < v[start].size(); i++)
-	{
-		if (!visit[v[start][i]])
-		{
-			DFS(v[start][i]);
+		if (!visited[nx]) {
+			dfs(nx);
 		}
 	}
-
-
 }
 
-// BFS -> 큐로 규현
-void BFS(int start)
-{
-	queue<int>q;
-	q.push(start);
-	visit[start] = true;
+void bfs(int curr) {
 
-	while (!q.empty())
-	{
+	queue<int> q;
+	q.push(curr);
+	visited[curr] = 1;
+
+	while (!q.empty()) {
 		int x = q.front();
 		q.pop();
-		bfs.push_back(x);
+		cout << x << " ";
 
-		for (int i = 0; i < v[x].size(); i++)
-		{
-			if (!visit[v[x][i]])
-			{
-				q.push(v[x][i]);
-				visit[v[x][i]] = true;
+		for (auto nx : graph[x]) {
+			if (!visited[nx]) {
+				q.push(nx);
+				visited[nx] = 1;
 			}
 		}
-
 	}
 
 }
 
-int main()
-{
+
+int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(0); cout.tie(0);
 
-	init();
-	DFS(V);
+	int from, to;
 
-	memset(visit, false, sizeof(visit));
-
-	BFS(V);
-
-	for (int i = 0; i < dfs.size(); i++)
-	{
-		cout << dfs[i] << " ";
+	cin >> n >> m >> v;
+	for (int i = 0; i < m; i++) {
+		cin >> from >> to;
+		graph[from].push_back(to);
+		graph[to].push_back(from);
 	}
+
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+
+
+	visited.assign(1001, 0);
+	dfs(v);
 	cout << '\n';
 
-	for (int i = 0; i < bfs.size(); i++)
-	{
-		cout << bfs[i] << " ";
-	}
+	visited.assign(1001, 0);
+	bfs(v);
+	cout << '\n';
 
 }

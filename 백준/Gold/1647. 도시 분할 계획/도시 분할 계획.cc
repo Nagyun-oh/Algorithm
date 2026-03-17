772bc388eb299fc3fe459640e07b75a01e36d56a
Nagyun <soahtmvhswl@gmail.com>
2026-03-17 15:18:14 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 316 ms, Memory: 20976 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-int N, M; // 집의 개수 N, 길의 개수 M
+int N, M; // 집의개수 N, 길의 개수 M
+int A, B, C; // A번 집과 B번 집을 연결하는 길의 유지비가 C
+
+int parent[100000+1];
 struct info {
 	int cost;
 	int a;
@@ -13,19 +15,16 @@ struct info {
 
 vector<info> v;
 
-int parent[100001];
-
 bool cmp(info a, info b) {
 	return a.cost < b.cost;
 }
 
 int findParent(int x) {
-	if (x == parent[x])return x;
+	if (x == parent[x]) return x;
 	return parent[x] = findParent(parent[x]);
 }
 
 void unionParent(int a, int b) {
-
 	a = findParent(a);
 	b = findParent(b);
 
@@ -37,54 +36,40 @@ int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(0); cin.tie(0);
 
-	int a, b, c;
-
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
-		cin >> a >> b >> c;
-		
-		v.push_back({ c,a,b });
+		cin >> A >> B >> C;
+		v.push_back({ C,A,B});
 	}
 
-	// 1. 비용 기준 오름차순으로 정렬
+	// 1. 비용 기준 오름차순 정렬
 	sort(v.begin(), v.end(), cmp);
 
-	// 2. 부모 배열 초기화
-	for(int i=1;i<=N;i++){
-		parent[i] = i; // 자기 자신으로 초기화
-	}
-
-	int ans = 0, cnt = 0;
-	int mx = 0;
-
+	// 2. 부모 배열 자기 자신으로 초기화
+	for (int i = 1; i <= N; i++)parent[i] = i;
 
 	// 3. 크루스칼 알고리즘
-	for (int i = 0; i < v.size(); i++) {
-		info curEdge = v[i];
-		int cost = curEdge.cost;
-		int now = curEdge.a;
-		int next = curEdge.b;
+	int mx = 0;
+	int ans = 0, cnt = 0;
+	for (auto x : v) {
+		int cost = x.cost;
+		int now = x.a;
+		int next = x.b;
 
-		if (cnt == N - 1)break; // 최소 모서리 개수 = 정점의 개수 - 1
+		if (cnt == N - 1)break;
 
-		// 사이클이면 패스
+		// 사이클 방지
 		if (findParent(now) == findParent(next))continue;
 
 		unionParent(now, next);
 
-		if (mx < cost) {
-			mx = cost;
-		}
-
+		ans += cost;
 		cnt++;
 
-		ans += cost;
+		if (mx < cost)mx = cost;
+
 	}
 
-
-	// ans : 최소한의 비용으로 길을 이어서 하나의 마을로 만들떄의 총 유지비
-	// mx : 그 길중 가장 비용이 큰 간선
-	// ans - mx : 가장 비용이 큰 간선을 빼면, 비용이 최소인 두 마을이 형성됨.
 	cout << ans - mx;
 
 }

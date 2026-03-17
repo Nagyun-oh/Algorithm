24fff109af86d6e089f4c26a88124d282ae19060
Nagyun <soahtmvhswl@gmail.com>
2026-03-17 15:31:00 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -3,36 +3,33 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> ary;
+int N, S;
+vector<int> ary(100000 + 1);
 
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(0); cin.tie(0);
 
-	/* 입력 */
-	int x;
-	int N, S;
 	cin >> N >> S;
 	for (int i = 0; i < N; i++) {
-		cin >> x;
-		ary.push_back(x);
+		cin >> ary[i];
 	}
 
-	/*  투 포인터 , 누적 합*/
-	int sum = 0, start = 0, end = 0;
-	int minLen = 2e9;
-
+	int start = 0,end = 0, sum = 0;
+	int mn = 2e9;
 	while (start <= end) {
 
 		if (sum >= S) {
-			minLen = min(minLen, end - start);
+			mn = min(mn, end - start);
 			sum -= ary[start++];
 		}
-		else if (end == N)break;
-		else sum += ary[end++];
+		else if (end == N) break;
+		else {
+			sum += ary[end++];
+		}
 	}
 
-	/* 출력 */
-	if (minLen != 2e9) cout << minLen;
+	if (mn != 2e9)cout << mn;
 	else cout << 0;
+
 }

eeb5048f289ff5417430b4d46ef19fc443e856d6
Nagyun <soahtmvhswl@gmail.com>
2026-03-26 13:55:58 +0900

    [Gold III] Title: 소수의 연속합, Time: 24 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,52 +1,55 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int N;	// 1<= N <= 4,000,000
-bool is_prime[4000000 + 1];
+int N;
 vector<int> primes;
+bool isPrimes[4000000 + 1];
 
 int main() {
 	ios::sync_with_stdio(false);
-	cout.tie(0); cin.tie(0);
-
+	std::cout.tie(0); cin.tie(0);
+	
 	cin >> N;
+	/* 1은 소수 합이 불가하므로 예외 처리 */
 	if (N == 1) {
-		cout << 0;
+		std::cout << 0;
 		return 0;
 	}
-	
-	// 에라토스테네스의 체 
-	
-	// 1) 2부터 N까지 모든 숫자 써넣기
-	for (int i = 2; i <= N; i++)is_prime[i] = true;	
 
-	// 2) 선택: 아직 지워지지 않은 수 중 가장 작은 수 P를 찾기
+	/* 에라토스테네스의 체*/
+
+	// 1. 일단 다 소수로 설정
+	for (int i = 2; i <= N; i++)isPrimes[i] = true;
 	for (int i = 2; i * i <= N; i++) {
-		// 3) 제거 : P를 제외한 P의 배수들을 모두 지움.
-		if (is_prime[i]) {
-			for (int j = i * i; j <= N; j += i)
-				is_prime[j] = false;	
+
+		// 2. 소수의 배수들 다 지워주기
+		if (isPrimes[i]) {
+			for (int j = i * i; j <= N; j += i) {
+				isPrimes[j] = false;
+			}
 		}
 	}
+	// 3. 추출한 소수로 배열 생성
 	for (int i = 2; i <= N; i++) {
-		if (is_prime[i])primes.push_back(i);
+		if (isPrimes[i])primes.push_back(i);
 	}
-	 
-	// 투 포인터
+
+	/* 투 포인터*/
 	int start = 0, end = 0, sum = 0, ans = 0;
-	int size = primes.size();
 	while (true) {
+
 		if (sum >= N) {
-			if (sum == N) ans++;
+			if (sum == N)ans++;
 			sum -= primes[start++];
-		}
-		else if (end == size) break;
-		else {
+		}else if (end == primes.size()){
+			break;
+		}else{
 			sum += primes[end++];
 		}
+		
 	}
 
-	cout << ans;
+	std::cout << ans;
+
 }

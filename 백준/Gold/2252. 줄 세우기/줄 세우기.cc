f7d5870df280dfa71696704d3be373f3a03e752c
Nagyun <soahtmvhswl@gmail.com>
2026-03-28 13:07:46 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 3956 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int N, M;	// 1<= N <=32,000 , 1<= M <=100,000
+vector<int> adj[32001];
+int inDegree[32001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(0); cin.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int A, B;
+		cin >> A >> B;
+		adj[A].push_back(B);
+		inDegree[B]++;
+	}
+
+	queue<int> q;
+	// 1. 진입 차수가 0인 학생들 먼저 큐에 삽입
+	for (int i = 1; i <= N; i++) {
+		if (inDegree[i] == 0)q.push(i);
+	}
+
+	// 2. 위상 정렬 시작
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		cout << cur << " ";
+
+		for (int next : adj[cur]) {
+			inDegree[next]--;
+			if (inDegree[next] == 0) {
+				q.push(next);
+			}
+		}
+	}
+
+}

49e3beea1cf2725eb9152a4aa29095b432a0d353
Nagyun <soahtmvhswl@gmail.com>
2026-03-29 12:23:42 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 32 ms, Memory: 19868 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -1,56 +1,53 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-vector<int> ary;
-int dp[100001][5][5];
+int ary[100001];
+int dp[100001][5][5]; // dp[지시사항][왼쪽][오른쪽]
+int n;
 
 int get_cost(int from, int to) {
 	if (from == 0)return 2;
-	else if (from == to)return 1;
-	else if ((from == 2 && to == 4) || (from == 4 && to == 2) || 
-		(from == 1 && to == 3) || (from == 3 && to == 1)) return 4;
+	else if (from == to) return 1;
+	else if ((from == 2 && to == 4) || (from == 4 && to == 2)
+		|| (from == 1 && to == 3) || (from == 3 && to == 1)) return 4;
 	else return 3;
 }
 
-int solve(int cnt, int left, int right) {
-	if (cnt == ary.size()) return 0;
-	if (dp[cnt][left][right] != -1) return dp[cnt][left][right];
+int solve(int cmd, int left, int right) {
+	if (cmd == n)return 0;
+	if (dp[cmd][left][right] != 0) return dp[cmd][left][right];
 
-	int target = ary[cnt];
+	int target = ary[cmd];
 
-	// 왼발을 움직이는 경우 (오른발과 겹치지 않아야함)
 	int move_left = 1e9;
 	if (target != right) {
-		move_left = solve(cnt + 1, target, right) + get_cost(left, target);
+		move_left = solve(cmd + 1, target, right) + get_cost(left, target);
 	}
-	// 오른발을 움직이는 경우 (왼발과 겹치지 않아야함)
+
 	int move_right = 1e9;
 	if (target != left) {
-		move_right = solve(cnt + 1, left, target) + get_cost(right, target);
+		move_right = solve(cmd + 1, left, target) + get_cost(right, target);
 	}
-	return dp[cnt][left][right] = min(move_left, move_right);
+
+	return dp[cmd][left][right] = min(move_left, move_right);
+
 }
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(0); cin.tie(0);
 
-	int i = 0;
 	while (true) {
-		int x;
-		cin >> x;
-		
-		if (x == 0)break;
-		else {
-			ary.push_back(x);
-			i++;
-		}
+		int val; cin >> val;
+		if (val == 0)break;
+		else ary[n++] = val;
 	}
 
-	memset(dp, -1, sizeof(dp));
+	memset(dp, 0, sizeof(dp));
+
 	cout << solve(0, 0, 0);
-	
 }

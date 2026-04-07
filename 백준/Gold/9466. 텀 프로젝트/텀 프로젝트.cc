b445407ef4998e8f018e3da78f67f8e48b9606d3
Nagyun <soahtmvhswl@gmail.com>
2026-04-07 20:07:10 +0900

    [Gold III] Title: 텀 프로젝트, Time: 444 ms, Memory: 7928 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int T;
+int n;
+int in_degree[100001];
+vector<int> adj[100001];
+
+void solve() {
+
+	queue<int> q;
+	int cnt = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (in_degree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		cnt++;
+
+		for (int next : adj[cur]) {
+			in_degree[next]--;
+			if (in_degree[next] == 0) {
+				q.push(next);
+			}
+		}
+		
+	}
+
+	cout << cnt << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(0); cin.tie(0);
+
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		
+		for (int i = 1; i <= n; i++) {
+			adj[i].clear();
+			in_degree[i] = 0;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			int to;
+			cin >> to;
+			adj[i].push_back(to);
+			in_degree[to]++;
+		}
+		solve();
+	}
+
+	return 0;
+}

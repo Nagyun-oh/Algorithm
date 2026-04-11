644b5ae6906a08e80e0d9bcf2c4188f3750fbad6
Nagyun <soahtmvhswl@gmail.com>
2026-04-11 15:16:39 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 20 ms, Memory: 6904 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.cc
@@ -3,53 +3,57 @@
 #include <algorithm>
 using namespace std;
 
-char mmap[1001][1001];
-int visited[1001][1001];
 int N, M;
+char mmap[1001][1001];
+int parent[1000*1000+1];
 int ans = 0;
 
-int direct[4][2] = {
-	-1,0,	// U	0
-	1,0,	// D	1
-	0,-1,	// L	2
-	0,1		// R	3
-};
-
-// SAFE-ZONE == 그래프의 구성요소
-int dir_index(int y,int x) {
-
-	if (mmap[y][x] == 'U') return 0;
-	else if (mmap[y][x] == 'D')	return 1;
-	else if (mmap[y][x] == 'L') return 2;
-	else if (mmap[y][x] == 'R') return 3;
+int getParent(int x) {
+	if (x == parent[x]) return x;
+	return parent[x] = getParent(parent[x]);
 }
 
-void dfs(int y, int x) {
+bool unionParent(int a, int b) {
+	a = getParent(a);
+	b = getParent(b);
 
-	visited[y][x] = 1;
-	
-	int dir = dir_index(y, x);
-	int dy = y + direct[dir][0];
-	int dx = x + direct[dir][1];
-	if (visited[dy][dx] == 0) dfs(dy, dx);
-	else if (visited[dy][dx] == 1) ans++;	// 사이클 발견
-	
-	visited[y][x] = 2;	
+	if (a != b) {
+		parent[b] = a;
+		return true;
+	}else return false;
 }
+
+int get_idx(int y, int x) {
+	return y * M + x;
+}
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cout.tie(0); cin.tie(0);
-	
-	string s;
+
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		cin >> mmap[i];
 	}
 
+	for (int i = 0; i < N * M; i++)parent[i] = i;
+
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (!visited[i][j])dfs(i, j);
+			int curr = get_idx(i, j);
+			int dy = i; int dx = j;
+
+			if (mmap[i][j] == 'U') dy--;
+			else if (mmap[i][j] == 'D')dy++;
+			else if (mmap[i][j] == 'L')dx--;
+			else if (mmap[i][j] == 'R')dx++;
+
+			int next_idx = get_idx(dy, dx);
+			// 사이클 발생
+			if (unionParent(curr, next_idx) == false) {
+				ans++;
+			}
 		}
 	}
 
